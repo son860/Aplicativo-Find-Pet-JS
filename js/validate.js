deb3c9f8e01716fca9e30767c3eb10501367578d
Emerson Garcia <emersomggarcia86@gmail.com>
2018-07-04 17:09:30 -0300

    Primeiro Commit
    Primeiro Commit do projeto

---
+++ js/validate.js
@@ -0,0 +1,22 @@
+//Validate ZIP Code
+export function isValidZip(zip){
+    return /^\d{5}(-\d{4})?$/.test(zip)
+}
+
+//Display Alert Message
+export function showMessage(message, className){
+   //Create div
+   const div = document.createElement('div');
+   //Add Classes
+   div.className = ` alert alert-${className}`;
+   //Add Text
+   div.appendChild(document.createTextNode(message));
+   //Get Container
+   const container = document.querySelector('.container');
+   //Get Form
+   const form = document.querySelector('#pet-form');
+   //Insert Alert
+   container.insertBefore(div, form);
+
+   setTimeout(() => document.querySelector('.alert').remove(), 3000);
+}
